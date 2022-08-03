36d4c4be72f41d00731b4698bdbc7fffdeea0223
ivan18gauna <ivan18gauna@gmail.com>
2022-08-03 20:57:46 -0300

    server.

--- api/src/app.js
+++ api/src/app.js
@@ -11,7 +11,7 @@ const cors = require("cors");
 
 server.name = "API";
 
-server.use(cors());
+express.server.use(cors());
 server.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
 server.use(bodyParser.json({ limit: "50mb" }));
 server.use(cookieParser());
