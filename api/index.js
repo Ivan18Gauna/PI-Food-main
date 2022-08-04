e602c86dd411cea4d8fa2770516fc5af870daba1
ivan18gauna <ivan18gauna@gmail.com>
2022-08-03 23:25:40 -0300

    va

--- api/index.js
+++ api/index.js
@@ -20,11 +20,6 @@
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
 
-//////
-require('dotenv').config();
-//////
-
-
 // Syncing all the models at once.
 conn.sync({ force: true }).then(() => {
   // server.listen(3001, () => {
