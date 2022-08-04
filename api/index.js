18217fd401a582d38cb1e1d040f2712799c1fd81
ivan18gauna <ivan18gauna@gmail.com>
2022-08-03 22:42:45 -0300

    pg client

--- api/index.js
+++ api/index.js
@@ -20,13 +20,6 @@
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
 
-//////
-const express = require('express')
-const cors = require('cors')
-const app = express()
-
-app.use(cors())
-//////
 
 
 // Syncing all the models at once.
